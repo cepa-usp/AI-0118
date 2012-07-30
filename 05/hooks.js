8cc8d6e17aa3144a9cc4b9e7fc3f1f2295f02f4c
Arthur Tofani <gramofone@gmail.com>
2012-07-30 19:15:36 -0300

    parte 05 e 06 em andamento

--- 05/hooks.js
+++ 05/hooks.js
@@ -1,3 +1,19 @@
+/**
+ *
+ A AI-0112 usa esses dois métodos via ExternalInterface
+
+ public function receivePointsFromJs(xmlstr:String){
+ unmarshalItems(xmlstr);
+ }
+
+ public function sendPointsToJS(){
+ return marshalItems();
+ }
+ * @type {*}
+ */
+
+
+
 var scorm = pipwerks.SCORM; // Seção SCORM
 scorm.version = "2004"; // Versão da API SCORM
 
@@ -5,52 +21,89 @@ var DEBUG = false; //MUDE DEBUG PARA TRUE PRA FACILITAR A NAVEGAÇÃO PELA ATIVI
 
 var oaName = "AI-0118_5"
 function AILocalStorageData() {
-    this.txResposta1 = "";
+    this.part_stop = 1;
+    this.complete_05 = false;
+    this.resp_05_plot = "";
+    this.score_05 = 0;
 }
 var ai_data;
 
 function startAI(){
-    loadScreen("../flash/AI-0112.swf", 640, 480);
+    loadScreen("../swf/AI-0112.swf", 640, 480);
     loadContent();
 
 }
 
 
+/**
+ Determinar o comportamento inicial da atividade que está sendo inserida
+ */
+function onInitialize(){
+    loadSlide('quadro1');
+
+}
+
+function start_quadro1(){
+    $("#btAbrirQuadro2").button().click(function (){
+        loadSlide("quadro2")
+    });
+}
+
+function start_quadro2(){
+    downScreen();
+    config_parada2();
+    $("#btAbrirQuadro3").button().click(function (){
+        loadSlide("quadro3")
+    });
+}
+
+function start_quadro3(){
+    $("#btAbrirQuadro31").button().click(function (){
+        loadSlide("quadro31")
+    });
+}
+
+function start_quadro31(){
+    $("#btAbrirQuadro4").button().click(function (){
+        loadSlide("quadro4")
+    });
+}
+
+
+function start_quadro4(){
+    $("#btAvaliarQuadro4").button().click(function (){
+        loadSlide("quadro5")
+    });
+}
+
+function start_quadro5(){
+    $("#btAbrirQuadro6").button().click(function (){
+        loadSlide("quadro6")
+    });
+}
+
+function start_quadro6(){
+    loadScreen("../swf/AI-0113.swf", 640, 480);
+    $("#btAbrirQuadro7").button().click(function (){
+        loadSlide("quadro7")
+    });
+}
+
+function start_quadro7(){
+    $("#btAbrirQuadro8").button().click(function (){
+        loadSlide("quadro8")
+    });
+}
+
+function start_quadro8(){
+
+}
+
 /*
  * Configurações DEPENDENTES do status da AI 
  */
 function posFetchConfig () {
-	//Parada 1
-	if(state.parte5.resp_05_01_verify == "")
-	{
-		$("#bt-05-01").click(function (){
-			$(".parada-2").removeClass('invisible').addClass('visible');
-			$("#bt-05-01").addClass('invisible');
-			state.parte5.resp_05_01_verify = "done";
-			ai = $("#ai-0112")[0];
-			config_parada2();
-		});
-	}else if(state.parte5.resp_05_01_verify == "done")
-	{
-		$(".parada-2").removeClass('invisible').addClass('visible');
-		$("#bt-05-01").addClass('invisible');
-		config_parada2();
-	}
-	
-	//Parada 2
-	if(state.parte5.resp_05_02_verify == "")
-	{
-		$("#bt-05-02").click(function(){
-			$(".parada-3").removeClass('invisible').addClass('visible');
-			$("#bt-05-02").addClass('invisible');
-			state.parte5.resp_05_02_verify = "done";
-		});
-	}else if(state.parte5.resp_05_02_verify == "done")
-	{	
-		$(".parada-3").removeClass('invisible').addClass('visible');
-		$("#bt-05-02").addClass('invisible');
-	}
-	
+
 	//Parada 3
 	if(state.parte5.resp_05_03_verify == "")
 	{
@@ -143,7 +196,7 @@ function posFetchConfig () {
 */
 function config_parada2()
 {
-	var aux = "<item label='y' value='1'><points><pt x='0' y='0' /><pt x='5' y='5' /></points></item>";
+	var aux = "<item label='y' value='1'><points><pt x='-5' y='-5' /><pt x='5' y='5' /></points></item>";
 	ai.receivePointsFromJs(aux);
 }
 
@@ -155,174 +208,3 @@ function config_parada6()
 	ai.receivePointsFromJs(nova_str);
 }
 
-function config_parada8()
-{
-	flashMovie2 = $('#movie-0113');
-
-	flashMovie2.flash(
-	{
-		swf: '../swf/AI-0113.swf',
-		width: 640,
-		height: 480,
-		play: false,
-		id: 'ai-0113'
-	}
-	);
-	
-	ai2 = $("#ai-0113")[0]; 
-}
- 
-/*
- * Avaliação da atividade
- */
-function evaluate(num)
-{
-	
-}
-
-/*
- * Finaliza a atividade
- */
-function finish () {
-
-	state.parte5.complete_05 = true;
-	
-	var success = commit(state);
-	
-	if (success) {
-		/*$("#enviar").button("option", "disabled", true);
-		$("#answer").button("option", "disabled", false);
-		$(".completion-message").show();
-		$("#after-finish-dialog").dialog("open");
-		$(".count").html(current_count);
-		$(".score").show();*/
-		alert("Go to next part");
-	}
-	else {
-		alert("Falha ao enviar dados para o LMS.");
-	}
-
-}
-
-/*
- * Inicia a conexão SCORM.
- */ 
-function fetch () {
- 
-	var ans = {};
-	//ans.completed = false;
-	//ans.try_completed = false;
-	//ans.score = 0;
-	//ans.learner = "";
-	//ans.connected = false;
-	ans.part = 5;
-	ans.parte5 = {};
-	ans.parte5.part_stop = 1;
-	ans.parte5.complete_05 = false;
-	ans.parte5.resp_05_01_verify = "";
-	ans.parte5.resp_05_02_verify = "";
-	ans.parte5.resp_05_03_verify = "";
-	ans.parte5.resp_05_04_verify = "";
-	ans.parte5.resp_05_05_verify = "";
-	ans.parte5.resp_05_06_verify = "";
-	ans.parte5.resp_05_07_verify = "";
-	ans.parte5.resp_05_plot = "";
-	ans.parte5.score_05 = 0;
-	
-	// Conecta-se ao LMS
-	session_connected = scorm.init();
-    
-	// Verifica se a AI já foi concluída.
-	var completionstatus = scorm.get("cmi.completion_status");
-
-	// A AI já foi concluída.
-	switch (completionstatus) {
-
-	  // Primeiro acesso à AI
-	  case "not attempted":
-	  case "unknown":
-	  default:
-		//ans.learner = scorm.get("cmi.learner_name");
-		//ans.completed = <valor padrão>;
-		//ans.try_completed = <valor padrão>;
-		//ans.count = <valor padrão>;
-		//ans.score = <valor padrão>;
-		//ans.choices = <valor padrão>;
-		ans.connected = session_connected;
-		//ans.standalone = session_standalone;
-		//ans.tries = <valor padrão>;
-		break;
-		
-	  // Continuando a AI...
-	  case "incomplete":
-		var stream = scorm.get("cmi.location");
-		if (stream != "") ans = JSON.parse(stream);
-		ans.part = 5;
-		//ans.learner = scorm.get("cmi.learner_name");
-		//ans.completed = false;
-		//ans.try_completed = false;
-		//ans.count = <obtido de cmi.location>;
-		//ans.score = <obtido de cmi.location>;
-		//ans.choices = <obtido de cmi.location>;
-		ans.connected = session_connected;
-		//ans.standalone = session_standalone;
-		//ans.tries = <obtido de cmi.location>;
-		break;
-		
-	  // A AI já foi completada.
-	  case "completed":
-		var stream = scorm.get("cmi.location");
-		if (stream != "") ans = JSON.parse(stream);
-		//ans.learner = scorm.get("cmi.learner_name");
-		//ans.completed = true;
-		//ans.try_completed = true;
-		//ans.count = <obtido de cmi.location>;
-		//ans.score = <obtido de cmi.location>;
-		//ans.choices = <obtido de cmi.location>;
-		ans.connected = session_connected;
-		//ans.standalone = session_standalone;
-		//ans.tries = <obtido de cmi.location>;
-		break;
-	}    
-  
-  return ans;
-}
-
-/*
- * Salva cmi.score.raw, cmi.location e cmi.completion_status no LMS
- */ 
-function commit (data) {
-
-  var success = false;
-
-  // Garante que a nota do usuário é um inteiro entre 0 e 100.
-  data.score = 100;
-  
-  // Se estiver rodando como stand-alone, usa local storage (HTML 5)
-  if (data.standalone) {
-	
-    var stream = JSON.stringify(data);
-    localStorage.setItem("ai_0006_redefor-memento", stream);
-    
-    success = true;
-  }
-  // Se estiver conectado a um LMS, usa SCORM
-  else {  
-
-    //if (scorm.connection.isActive) {
-    if (data.connected) {
-    
-      // Salva no LMS a nota do aluno.
-      success = scorm.set("cmi.score.raw", data.score);
-      
-      // Salva no LMS o status da atividade: completada ou não.
-      success = scorm.set("cmi.completion_status", (data.completed ? "completed" : "incomplete"));
-      
-      // Salva no LMS os demais dados da atividade.
-      var stream = JSON.stringify(data);      
-      success = scorm.set("cmi.location", stream);
-    }
-  }
-  
-  return success;
-}
