d65b4c75ec7808f951a08c0d8a03443936baf111
Mary Vigna <mavigna@gmail.com>
2012-08-01 16:40:33 -0300

    Exercício 3 - faltam ajustes no design e na interação com a ai114

--- 03/hooks.js
+++ 03/hooks.js
@@ -8,6 +8,7 @@
 var DEBUG = false; //MUDE DEBUG PARA TRUE PRA FACILITAR A NAVEGAÇÃO PELA ATIVIDADE!!!
 
 var oaName = "AI-0118_3"
+
 function AILocalStorageData() {
 	this.cp1 = "";
     this.cp2 = "";
@@ -18,9 +19,13 @@ var ai_data;
 
 function startAI(){
     oaData = fetch();
+	state = fetch();
+	
     if(oaData.slides==undefined) oaData.slides = new AILocalStorageData();
     loadScreen("../swf/AI-0114.swf", 640, 480);
 	loadContent();
+	
+	
 }
 
 
@@ -32,7 +37,6 @@ function onInitialize(){
 
 	loadSlide("quadro1");
 	
-	
 }
 
 
@@ -40,7 +44,7 @@ function start_quadro1(){
 	
 	// popular o slide com as variáveis colhidas do scorm;
 		
-	$("#bt-03-01").button().click(function(){
+	$("#avancar1").button().click(function(){
 		loadSlide("quadro2");
 
 	});	
@@ -48,85 +52,373 @@ function start_quadro1(){
 
 
 
-
-
-
 function start_quadro2(){
-    $("#errou01").hide();
+	$("#parada-2").hide();
+	$("#parada-3").hide();
+	$("#parada-4").hide();
+	$("#parada-5").hide();
+    $("#bt-03-01").button().click(function(){
+	//Verificando se o ponto na atividade está dentro ou fora do domínio
+	if(movie.insideDomain())
+	{
+		$("#parada-2").show();
+		disableElement("#bt-03-01");
+		}else{
+			alert("O ponto (x,y) está fora do domínio. Posicione novamente.");
+			return;
+		}
+    });
+	$("#bt-03-02").button().click(function(){
+        $("#parada-3").show();
+		disableElement("#bt-03-02");
+    });
+	$("#bt-03-03").button().click(function(){
+        $("#parada-4").show();
+		disableElement("#bt-03-03");
+    });	
+	$("#bt-03-04").button().click(function(){
+        $("#parada-5").show();
+		disableElement("#bt-03-04");
+    });	
+	$("#bt-03-05").button().click(function(){
+		movie.setDomainClosed(true);// Verificar a função que coloca x = 0 e y = 0 no domínio e reinicializa a atividade
+        loadSlide("quadro3");
+		disableElement("#bt-03-05");
+    });
+}
+
+function start_quadro3(){
+    $("#parada-7").hide();
+    $("#parada-8").hide();
+    $("#parada-9").hide();	
+	$("#parada-10").hide();
+    $("#parada-11").hide();
+	$("#parada-12").hide();
+	$("#parada-13").hide();
+	$("#errou01").hide();
     $("#acertou01").hide();
-    $("#resp").hide();
+	$("#errou02").hide();
+    $("#acertou02").hide();
+	$("#errou03").hide();
+    $("#acertou03").hide();
+	$("#acertou03-1").hide();
+    $("#acertou03-2").hide();
+	$("#acertou03-3").hide();
+    $("#acertou03-4").hide();
+	$("#errou03-1").hide();
+    $("#errou03-2").hide();
+	$("#errou03-3").hide();
+    $("#errou03-4").hide();
+	
+    $("#bt-03-06").button().click(function(){
+		if(movie.onBorder())// Função que verifica se o ponto está ou não na fronteira
+		{
+		$("#parada-7").show();
+		disableElement("#bt-03-06");
+		}else {
+			/*alert("O ponto (x,y) está fora da borda. Posicione novamente.\nDica: Arraste o ponto para a borda da função até que o eixo fique vermelho.");*/
+			//Confirm box de avaliação do ponto na fronteira
+			
+			$("#dialog-confirm").dialog({
+			
+				resizable: false,
+				modal: true,
+				position: ['center',530],
+				buttons: {
+					"Posicione (x,y) por mim": function() {
+						movie.setPos(2,0);// Verificar função que posiciona ponto na fronteira(2,0)					
+						$(this).dialog( "close" );
+					},
+					"Ok": function() {
+						$(this).dialog( "close" );
+					}
+				}
+			});
+		return;	
+		}
+    });
+	
+    $("#bt-03-07").button().click(function(){
+        $("#parada-8").show();
+		disableElement("#bt-03-07");
+    });
+	
+    $("#bt-03-08").button().click(function(){
+		$( "#sl-03-dom-01" ).attr("disabled",true);
+	
+		//Avaliar o select	
+		if($("#sl-03-dom-01").val() == 'fechado')
+		{
+			console.log("correto");
+			oaData.slides.score_02_02 = 100;
+			 $("#acertou01").show();
+		}else 
+		{
+			console.log("errado");
+			 $("#errou01").show();
+			oaData.slides.score_02_02 = 0;
+		}
+			oaData.slides.resp_03_03_01 = $("select[name='sl-03-dom-01'] :selected").val();
+	
+		$("#parada-9").show();
+		disableElement("#bt-03-08");		
+	});	
+		
+	$("#bt-03-09").button().click(function(){
+		$( "#sl-03-dom-02" ).attr("disabled",true);
+		//App altera o dominio 
+		
+		//Avaliar o select	
+		if($("#sl-03-dom-02").val() == 'aberto')
+		{
+			console.log("correto");
+			oaData.slides.score_02_03 = 100;
+			 $("#acertou02").show();
+		}else 
+		{
+			console.log("errado");
+			 $("#errou02").show();
+			oaData.slides.score_02_03 = 0;
+		}
+			oaData.slides.resp_03_03_02 = $("select[name='sl-03-dom-02'] :selected").val();
+		
+			$("#parada-10").show();
+			disableElement("#bt-03-09");		
+	});	
+	    
+	$("#bt-03-10").button().click(function(){
+		$("#parada-11").show();
+		disableElement("#bt-03-10");
+    });
+	
+	$("#bt-03-11").button().click(function(){
+		$("#parada-12").show();
+		$( "#sl-03-dom-03" ).attr("disabled",true);
+		
+		//Avaliar o select	
+		if($("#sl-03-dom-03").val() == 'aberto') 
+		{
+			console.log("correto");
+			oaData.slides.score_02_03 = 100;
+			 $("#acertou03").show();
+		}else 
+		{
+			console.log("errado");
+			 $("#errou03").show();
+			oaData.slides.score_02_03 = 0;
+		}
+		disableElement("#bt-03-11");
+    });
 
-    $("#btToSlide3").button().click(function(){
-        loadSlide("quadro4")
+	$("#bt-03-12").button().click(function(){
+		$("#parada-13").show();
+		$( "#sl-03-dom-04" ).attr("disabled",true);
+		$( "#sl-03-dom-05" ).attr("disabled",true);
+		$( "#sl-03-dom-06" ).attr("disabled",true);
+		$( "#sl-03-dom-07" ).attr("disabled",true);
+		disableElement("#bt-03-12");
+	
+		//Avaliar o select	
+		if($("#sl-03-dom-04").val() == 'aberto') 
+		{
+			console.log("correto");
+			oaData.slides.score_02_03 = 100/4;
+			 $("#acertou03-1").show();
+		}else 
+		{
+			console.log("errado");
+			 $("#errou03-1").show();
+			oaData.slides.score_02_03 = 0;
+		}
+
+		//Avaliar o select	
+		if($("#sl-03-dom-05").val() == 'fechado') 
+		{
+			console.log("correto");
+			oaData.slides.score_02_03 = 100/4;
+			 $("#acertou03-2").show();
+		}else 
+		{
+			console.log("errado");
+			 $("#errou03-2").show();
+			oaData.slides.score_02_03 = 0;
+		}	
+		
+		//Avaliar o select	
+		if($("#sl-03-dom-06").val() == 'aberto') 
+		{
+			console.log("correto");
+			oaData.slides.score_02_03 = 100/4;
+			 $("#acertou03-3").show();
+		}else 
+		{
+			console.log("errado");
+			 $("#errou03-3").show();
+			oaData.slides.score_02_03 = 0;
+		}	
+	
+		//Avaliar o select	
+		if($("#sl-03-dom-07").val() == 'fechado') 
+		{
+			console.log("correto");
+			oaData.slides.score_02_03 = 100/4;
+			 $("#acertou03-4").show();
+		}else 
+		{
+			console.log("errado");
+			 $("#errou03-4").show();
+			oaData.slides.score_02_03 = 0;
+		}
+
     });
-    $("#bt-02-02").button().click(function(){
-        avaliarQuadro2()
-    });
+
 }
 
-function avaliarQuadro2() {
-
-    if($("select[name='cp1'] :selected").val() == "maior" && $("select[name='cp2'] :selected").val() == "0" && $("select[name='cp3'] :selected").val() == "maior" && $("select[name='cp4'] :selected").val() == "0")
-    {
-        //console.log("correto");
-        oaData.slides.score_02_02 = 100;
-    }else if($("select[name='cp1'] :selected").val() == "maiorigual" && $("select[name='cp2'] :selected").val() == "0" && $("select[name='cp3'] :selected").val() == "maior" && $("select[name='cp4'] :selected").val() == "0")
-    {
-        //console.log("correto/2-1");
-        oaData.slides.score_02_02 = 90;
-    }else if($("select[name='cp1'] :selected").val() == "maior" && $("select[name='cp2'] :selected").val() == "0" && $("select[name='cp3'] :selected").val() == "maiorigual" && $("select[name='cp4'] :selected").val() == "0")
-    {
-        //console.log("correto/2-2");
-        oaData.slides.score_02_02 = 90;
-    }else if($("select[name='cp1'] :selected").val() == "maiorigual" && $("select[name='cp2'] :selected").val() == "0" && $("select[name='cp3'] :selected").val() == "maiorigual" && $("select[name='cp4'] :selected").val() == "0")
-    {
-        //console.log("correto/2-3");
-
-        oaData.slides.score_02_02 = 80;
-    }else
-    {
-        //console.log("errado");
-
-        oaData.slides.score_02_02 = 0;
-    }
-    //console.log(oaData.slides.score_02_02);
-    //console.log("saindo");
-    //Armazenando os valores no array de avaliação dos selects
-    oaData.slides.resp_02_02_01 = $("select[name='cp1'] :selected").val();
-    oaData.slides.resp_02_02_02 = $("select[name='cp2'] :selected").val();
-    oaData.slides.resp_02_02_03 = $("select[name='cp3'] :selected").val();
-    oaData.slides.resp_02_02_04 = $("select[name='cp4'] :selected").val();
-
-    //Atualizando a visibilidade da próxima parte
-    disableElement("cp1");
-    disableElement("cp2");
-    disableElement("cp3");
-    disableElement("cp4");
-    disableElement("#bt-02-02");
-
-
-
-
-
-        loadScreen("../swf/ai-0119.swf", 640, 480)
-    alert(oaData.slides.score_02_02)
-
-
-    if(oaData.slides.score_02_02 < 100)
-    {
-        $("#errou01").show();
-    }else{
-        $("#acertou01").show();
-    }
-    $("#resp").show();
-
-}
 
 function notificar(tx){
 	//TODO: substituir o 'alert' por um pseudo pop-up (?)
 	alert(tx);
 }
 
+
+/*
+ * Inicia a conexão SCORM.
+ */ 
+function fetch () {
+ 
+	var ans = {};
+	ans.completed = false;
+	//ans.try_completed = false;
+	//ans.score = 0;
+	//ans.learner = "";
+	//ans.connected = false;
+	ans.part = 3;
+	ans.parte3 = {};
+	ans.parte3.part_stop = 1;
+	ans.parte3.complete_03 = false;
+	ans.parte3.resp_03_01_verify = "";
+	ans.parte3.resp_03_02_verify = "";
+	ans.parte3.resp_03_03_verify = "";
+	ans.parte3.resp_03_04_verify = "";
+	ans.parte3.resp_03_05_verify = "";
+	ans.parte3.resp_03_06_verify = "";
+	ans.parte3.resp_03_07_verify = "";
+	ans.parte3.resp_03_08_verify = "";
+	ans.parte3.resp_03_09_verify = "";
+	ans.parte3.resp_03_10_verify = "";
+	ans.parte3.resp_03_11_verify = "";
+	ans.parte3.resp_03_12_verify = "";
+	ans.parte3.resp_03_13_verify = "";
+	ans.parte3.score_03_08 = 0;
+	ans.parte3.resp_03_08_01 = "";
+	ans.parte3.score_03_09 = 0;
+	ans.parte3.resp_03_09_01 = "";
+	ans.parte3.score_03_12 = 0;
+	ans.parte3.resp_03_12_01 = "";
+	ans.parte3.score_03_13 = 0;
+	ans.parte3.resp_03_13_01 = "";
+	ans.parte3.resp_03_13_02 = "";
+	ans.parte3.resp_03_13_03 = "";
+	ans.parte3.resp_03_13_04 = "";
+	ans.parte3.score_03 = 0;
+	
+	// Conecta-se ao LMS
+	session_connected = scorm.init();
+    
+	// Verifica se a AI já foi concluída.
+	var completionstatus = scorm.get("cmi.completion_status");
+	//console.log(completionstatus);
+	// A AI já foi concluída.
+	switch (completionstatus) {
+
+	  // Primeiro acesso à AI
+	  case "not attempted":
+	  case "unknown":
+	  default:
+		//ans.learner = scorm.get("cmi.learner_name");
+		ans.completed = false;
+		//ans.try_completed = <valor padrão>;
+		//ans.count = <valor padrão>;
+		//ans.score = <valor padrão>;
+		//ans.choices = <valor padrão>;
+		ans.connected = session_connected;
+		//ans.standalone = session_standalone;
+		//ans.tries = <valor padrão>;
+		break;
+		
+	  // Continuando a AI...
+	  case "incomplete":
+		var stream = scorm.get("cmi.location");
+		if (stream != "") ans = JSON.parse(stream);
+		ans.part = 3;
+		//ans.learner = scorm.get("cmi.learner_name");
+		ans.completed = false;
+		//ans.try_completed = false;
+		//ans.count = <obtido de cmi.location>;
+		//ans.score = <obtido de cmi.location>;
+		//ans.choices = <obtido de cmi.location>;
+		ans.connected = session_connected;
+		//ans.standalone = session_standalone;
+		//ans.tries = <obtido de cmi.location>;
+		break;
+		
+	  // A AI já foi completada.
+	  case "completed":
+		var stream = scorm.get("cmi.location");
+		if (stream != "") ans = JSON.parse(stream);
+		//ans.learner = scorm.get("cmi.learner_name");
+		ans.completed = true;
+		//ans.try_completed = true;
+		//ans.count = <obtido de cmi.location>;
+		//ans.score = <obtido de cmi.location>;
+		//ans.choices = <obtido de cmi.location>;
+		ans.connected = session_connected;
+		//ans.standalone = session_standalone;
+		//ans.tries = <obtido de cmi.location>;
+		break;
+	}    
+  
+  return ans;
+}
+
+/*
+ * Salva cmi.score.raw, cmi.location e cmi.completion_status no LMS
+ */ 
+function commit (data) {
+
+  var success = false;
+
+  // Garante que a nota do usuário é um inteiro entre 0 e 100.
+  data.score = (state.parte3.score_03_08+state.parte3.score_03_09+state.parte3.score_03_12+state.parte3.score_03_13)/4;
+  
+  // Se estiver rodando como stand-alone, usa local storage (HTML 5)
+  if (data.standalone) {
+	
+    var stream = JSON.stringify(data);
+    localStorage.setItem("ai_0006_redefor-memento", stream);
+    
+    success = true;
+  }
+  // Se estiver conectado a um LMS, usa SCORM
+  else {  
+
+    //if (scorm.connection.isActive) {
+    if (data.connected) {
+    
+      // Salva no LMS a nota do aluno.
+      success = scorm.set("cmi.score.raw", data.score);
+      
+      // Salva no LMS o status da atividade: completada ou não.
+      success = scorm.set("cmi.completion_status", (data.completed ? "completed" : "incomplete"));
+      
+      // Salva no LMS os demais dados da atividade.
+      var stream = JSON.stringify(data);      
+      success = scorm.set("cmi.location", stream);
+    }
+  }
+  
+  return success;
+}
 
 // Checks if given selector (type input) is a valid number. If not, resets field.
 function validateAnswer (selector) {
@@ -135,5 +427,6 @@ function validateAnswer (selector) {
   if (!isValid) $(selector).val("");
   return isValid;
 }
+
 
  
